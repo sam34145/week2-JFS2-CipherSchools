7a88ff263b1b091d16cbe0f5c4f0eb73c4e71680
sam34145 <117763565+sam34145@users.noreply.github.com>
2023-02-04 20:07:16 +0530

    Create Main0.java

---
+++ Main0.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hey buddy how old are you? ");
+        int age = sc.nextInt();
+
+        System.out.println("Woha!! You have really grown up!! you are " + age + " years old");
+    }
+}
