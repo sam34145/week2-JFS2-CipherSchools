82c39d98c047bee54ac12ee6018c80667c15c088
sam34145 <117763565+sam34145@users.noreply.github.com>
2023-02-04 20:09:12 +0530

    Create Main3.java

---
+++ Main3.java
@@ -0,0 +1,22 @@
+final class father{
+
+    public final String lastName = "obroi";
+    public void foodHabit(){
+        System.out.println("I like salty!!");
+    }
+}
+
+class son {
+
+//    public void foodHabit() {
+////        super.foodHabit();
+//        lastName = "Malhotra";
+//        System.out.println("I like sweet");
+//    }
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+
+    }
+}
